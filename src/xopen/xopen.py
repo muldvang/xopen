5281a3784a4730ad8a4b47656a32883e5cfcee46
Torben Muldvang Andersen - QIAGEN <Torben.Andersen@qiagen.com>
2025-07-10 23:15:17 +0200

    Support for configuration based on file extensions

--- src/xopen/xopen.py
+++ src/xopen/xopen.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python3
 
 import argparse
-import configparser
 import os
 import subprocess
 import sys
 from pathlib import Path
 
 import magic
+import yaml
 
 # TODO run black and ruff with poetry
 # TODO write automated tests
+# TODO create a bash wrapper. Does Poetry have something to suppor this going in /bin?
 # TODO ensure proper wildcard support
 # TODO make a fallback category?
 
 CONFIG_PATHS = [
     os.getenv("XOPEN_CONFIG"),
     # TODO add support for the XDG config dirs and friends: https://specifications.freedesktop.org/basedir-spec/latest/
-    Path.home() / ".config" / "xopen" / "config.ini"
+    Path.home() / ".config" / "xopen" / "config.yml",
 ]
 
 
 def load_config():
-    available_config_paths = [path for path in CONFIG_PATHS if path and os.path.exists(path)]
+    available_config_paths = [
+        path for path in CONFIG_PATHS if path and os.path.exists(path)
+    ]
     try:
         config_path = available_config_paths[0]
     except IndexError:
         print("Configuration file not found.")
         sys.exit(1)
-    config = configparser.ConfigParser()
-    config.read(config_path)
-    return config
+    with open(config_path) as file:
+        return yaml.safe_load(file)
 
 
 def get_mime_type(file_path):
@@ -38,14 +40,22 @@ def get_mime_type(file_path):
     return mime.from_file(file_path)
 
 
-def get_application(config, mode, mime_type):
-    if config.has_option(mode, mime_type):
-        return config.get(mode, mime_type)
-
-    main_type = mime_type.split('/')[0]
-    wildcard_key = f"{main_type}/*"
-    if config.has_option(mode, wildcard_key):
-        return config.get(mode, wildcard_key)
+def get_application(config, mode, mime_type, filepath):
+    print(config)
+    print(mime_type)
+    print(mode)
+    relevant_configs = (
+        entry
+        for entry in config
+        if mode in entry["mode"]
+        and (
+            ("mime" in entry and mime_type == entry["mime"])
+            or ("extension" in entry and filepath.endswith(entry["extension"]))
+        )
+    )
+    first_relevant_config = next(relevant_configs)
+    if first_relevant_config:
+        return first_relevant_config["mode"][mode]
 
     return None
 
@@ -53,21 +63,19 @@ def get_application(config, mode, mime_type):
 def main():
     # TODO support opening multiple files through multiple arguments
     parser = argparse.ArgumentParser(description="Open a file with the desired mode.")
-    parser.add_argument( "mode",
-        help="Mode of open: 'view' or 'edit'"
-    )
-    parser.add_argument( "filepath",
-        help="Path of the file to open"
-    )
+    parser.add_argument("mode", help="Mode of open: 'view' or 'edit'")
+    parser.add_argument("filepath", help="Path of the file to open")
 
     args = parser.parse_args()
     config = load_config()
     mime_type = get_mime_type(args.filepath)
-    app = get_application(config, args.mode, mime_type)
+    app = get_application(config, args.mode, mime_type, args.filepath)
 
     if not app:
         # TODO support fallback
-        print(f"No application configured for mode '{args.mode}' and MIME type '{mime_type}'")
+        print(
+            f"No application configured for mode '{args.mode}' for file '{args.filepath}'"
+        )
         sys.exit(1)
 
     try:
